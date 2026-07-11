0c7be2dd94f87df9d3096b45254ca8b8ae452a18
Aaron Dettmann <aaron.dettmann@gmail.com>
2026-07-11 23:13:44 +0200

    Improve Neovim autocomplete behavior

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -7,18 +7,51 @@
 return function(gh)
   vim.pack.add({
     gh("L3MON4D3/LuaSnip"),
+    gh("rafamadriz/friendly-snippets"),
     gh("saghen/blink.cmp"),
   })
 
   require("luasnip").setup({})
+  require("luasnip.loaders.from_vscode").lazy_load()
 
   require("blink.cmp").setup({
-    keymap = { preset = "default" },
-    appearance = { nerd_font_variant = "mono" },
-    sources = {
-      default = { "lsp", "path", "snippets" },
+    keymap = {
+      preset = "super-tab",
+      ["<CR>"] = { "accept", "fallback" },
     },
-    snippets = { preset = "luasnip" },
-    signature = { enabled = true },
+
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+
+    sources = {
+      default = {
+        "lsp",
+        "buffer",
+        "path",
+        "snippets",
+      },
+    },
+
+    snippets = {
+      preset = "luasnip",
+    },
+
+    completion = {
+      menu = {
+        auto_show = true,
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      ghost_text = {
+        enabled = true,
+      },
+    },
+
+    signature = {
+      enabled = true,
+    },
   })
 end
